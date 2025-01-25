d7d0328f752463c2327e67276dfd82d14e039b9a
Tommi Tuovinen <tommi.s.tuovinen@gmail.com>
2025-01-25 15:05:32 +0200

    Add first gate

--- main.js
+++ main.js
@@ -206,7 +206,13 @@ plane.layers.enable(0);
 camera.add(plane);
 
 
-
+const gates = [];
+const geometry_torus = new THREE.TorusGeometry( 0.5, 0.05, 16, 100 ); 
+const material_torus = new THREE.MeshBasicMaterial( { color: 0xffff00 } ); 
+const torus = new THREE.Mesh( geometry_torus, material_torus );
+gates.push(torus);
+torus.position.set(0,0,0);
+scene.add( torus );
 
 const VRCamera = renderer.xr.getCamera();
 //console.log(VRCamera);
