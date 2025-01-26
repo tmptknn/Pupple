4625baa135c3f7e1967fe3a769014d4bc7b736f8
Tommi Tuovinen <tommi.s.tuovinen@gmail.com>
2025-01-26 12:38:30 +0200

    Add music

--- main.js
+++ main.js
@@ -642,3 +642,19 @@ function render(time) {
   renderer.render(scene, camera);
   //composer.render();
 }
+
+// create an AudioListener and add it to the camera
+const listener = new THREE.AudioListener();
+camera.add(listener);
+
+// create a global audio source
+const sound = new THREE.Audio(listener);
+
+// load a sound and set it as the Audio object's buffer
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load("./music/calming_music.ogg", function (buffer) {
+  sound.setBuffer(buffer);
+  sound.setLoop(true);
+  sound.setVolume(0.5);
+  sound.play();
+});
